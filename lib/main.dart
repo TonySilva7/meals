2bed8b92ecf3c7a5264a3f9ff5915839b3534aad
TonySilva7 <tony.deve7oper@gmail.com>
2022-11-13 13:42:56 -0300

    feat: implements tabBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
+import 'package:meals/screens/tabs_screen.dart';
 import 'package:meals/utils/app_routes.dart';
 
 void main() => runApp(MyApp());
@@ -34,23 +35,10 @@ class MyApp extends StatelessWidget {
       ),
       // home: const CategoriesScreen(),
       routes: {
-        AppRoutes.home: (context) => const CategoriesScreen(),
+        AppRoutes.home: (context) => const TabsScreen(),
         AppRoutes.categoriesMeals: (ctx) => const CategoriesMealsScreen(),
         AppRoutes.mealDetail: (ctx) => const MealDetailScreen(),
       },
-      // onGenerateRoute: (configs) {
-      //   if (configs.name == '/alguma-coisa') {
-      //     return null;
-      //   } else if (configs.name == '/outra-coisa') {
-      //     return null;
-      //   } else {
-      //     return MaterialPageRoute(
-      //       builder: (_) {
-      //         return const CategoriesScreen();
-      //       },
-      //     );
-      //   }
-      // },
       onUnknownRoute: (configs) {
         return MaterialPageRoute(
           builder: (_) {
